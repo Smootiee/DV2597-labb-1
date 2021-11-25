9b2a9b5e4bc1eaa4786d3d6165261d4c38efceb2
smootie <sea-back@hotmail.com>
2021-11-25 21:06:02 +0100

    commented the code

--- qsortpar.c
+++ qsortpar.c
@@ -79,6 +79,8 @@ void * quick_sort(void * orgArgs)
 {
     struct workerArgs* args = (struct workerArgs*) orgArgs;
     unsigned pivot_index;
+    // Creating two threads that will get one part of the array, a low part and a high part
+    // Split in the middle.
     pthread_t worker_one, worker_two;
 
     /* no need to sort a vector of zero or one element */
@@ -90,6 +92,8 @@ void * quick_sort(void * orgArgs)
 
     pivot_index = partition(args->arr, args->low, args->high, pivot_index);
     /* partition the vector */
+
+    // Two structs created since the high and low respectivly will be different due to pivot_index -1 or +1.
     struct workerArgs args_one, args_two;
     args_one.arr = args->arr;
     args_one.low = args->low;
@@ -102,6 +106,7 @@ void * quick_sort(void * orgArgs)
     args_two.high = args->high;
     args_two.worker_count = args->worker_count + 2;
 
+    // Thread created in regards of how many workers were stated in the inparameters
     if (args->worker_count < numWorkers){
         if (args->low < pivot_index){
             pthread_create(&(worker_one), NULL, quick_sort, &args_one);
@@ -109,6 +114,7 @@ void * quick_sort(void * orgArgs)
         if (pivot_index < args->high){
             pthread_create(&(worker_two), NULL, quick_sort, &args_two);
         }
+        // Joins to wait for the threads to be done, so that the main thread wont move on.
         pthread_join(worker_one, NULL);
         pthread_join(worker_two, NULL);
 
@@ -117,6 +123,7 @@ void * quick_sort(void * orgArgs)
 
 
         /* sort the two sub arrays */
+        // Sequential sort of the array.
         if (args->low < pivot_index)
             quick_sort(&args_one);
         if (pivot_index < args->high)
@@ -125,6 +132,7 @@ void * quick_sort(void * orgArgs)
 
 }
 
+// Just a quick function to check if they array is sorted or not.
 char* validate_sorted(){
     for(int i = 0; i < MAX_ITEMS - 1; i++){
         if(v[i] > v[i+1]){
@@ -138,12 +146,16 @@ char* validate_sorted(){
 int
 main(int argc, char **argv)
 {
+    // Inparameter of how many workers should be used.
     if (argc > 1)
         numWorkers = atoi(argv[1]);
 
     struct workerArgs orgArg;
 
     init_array();
+
+    // In order to keep track of the data in a easy way and sending it into the thread.
+    // This struct is created with the needed data.
     orgArg.worker_count = 0;
     orgArg.low = 0;
     orgArg.high = MAX_ITEMS - 1;
